dbfc0242db3fdafbaa0aeef3bef4cb298a3b1c6e
Darrell Goodwin II <delanogoodwin21@gmail.com>
2026-02-08 04:23:24 -0500

    Initial commit: Flask app containerized with Docker

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps Project 1: Docker + CI/CD is LIVE 🚀"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
